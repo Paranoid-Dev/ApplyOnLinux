00c143ca4f49c37f3e732e0363d792932adf956b
Paranoid-Dev <du.brisingr.arget@gmail.com>
2021-09-13 16:30:26 +0900

    added more useragents

--- Jinhakapply.py
+++ Jinhakapply.py
@@ -7,7 +7,9 @@ chrome = webdriver.Chrome(options=options, executable_path='./chromedriver')
 ogagent = chrome.execute_script("return navigator.userAgent")
 chrome.execute_cdp_cmd("Network.setUserAgentOverride",
 	{
+#blocked		"userAgent": re.sub('\(.*?\)', '(Windows NT 10.0; Win64; x64)', ogagent, 1),
 		"userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+#works		"userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
 	},
 		)
 chrome.get("https://applymem.jinhakapply.com/Login")
